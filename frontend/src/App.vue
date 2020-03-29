814cc3fa8c89a0b24d3474d396a3c3790f9a4803
Luther13 <poryckim@gmail.com>
2020-03-29 20:25:18 +0200

    Create menubar.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Menu />
+    <router-view/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Menu from './components/menubar.vue'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    components: {
+      Menu
+    }
 }
 </script>
 
@@ -20,10 +18,9 @@ export default {
 @import url('https://fonts.googleapis.com/css?family=Archivo&display=swap');
 #app {
   font-family: 'Archivo', sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #6998a3;
+  margin: -7px;
 }
+
 </style>
