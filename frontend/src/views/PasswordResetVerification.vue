41eb11ac374d8e4ea596822d5864066a600660f4
MPorycki <poryckim@gmail.com>
2020-04-16 09:20:32 +0200

    PasswordResetVerification data validation

--- frontend/src/views/PasswordResetVerification.vue
+++ frontend/src/views/PasswordResetVerification.vue
@@ -7,6 +7,7 @@
                     <form @submit.prevent="send_link">
                         <div class="form-group">
                             <input type="text" id="email" class="form-control" placeholder="Email" value="" />
+                            <b-tooltip target="email" id="email_error" class="rtooltips" triggers="hover" placement="right"  :disabled=true >{{email_text}}</b-tooltip>
                         </div>
                         <div class="form-group">
                             <input type="submit" class="btnSubmit" value="Wyślij link" />
@@ -21,10 +22,45 @@
 import axios from 'axios'
 
 export default {
+    data(){
+        return {email_text: ""}
+    },
     methods:{
         send_link(){
             var data = {"email": document.getElementById("email").value}
-            axios.patch(this.$backend_url + 'account/reset_link', data).then(this.reset_link_sent()).catch(this.reset_link_sent_fail())
+            if (this.validate_email(data["email"])){
+                axios.patch(this.$backend_url + 'account/reset_link', data).then(this.reset_link_sent()).catch(this.reset_link_sent_fail())
+            }
+        },
+        validate_email(email){
+             var email_regex = /.*@.*\.[a-z]{2,}/;
+            if ((email.replace(/\s/g, '')).length > 32) {
+                this.add_error("email", "Podany email jest za długi")
+                return false;
+            } else if ((email.replace(/\s/g, '')).length == 0){
+                return false;
+            } else if (!email_regex.test(email)){
+                this.add_error("email", "Proszę wpisać email w poprawnym formacie")
+                return false;
+            } else {
+                return true;
+            }
+        },
+        add_error(element_name, error_text){
+            var form = document.getElementById(element_name);
+            form.style.borderColor = "red"
+            this.$root.$emit('bv::enable::tooltip', element_name + '_error')
+            this.$root.$emit('bv::show::tooltip', element_name+ '_error')
+            switch(element_name){
+                case "email":
+                    this.email_text = error_text
+                    break
+                }
+        },
+        clear_error(element_name){
+            var form = document.getElementById(element_name);
+            form.style.borderColor = "#ced4da"
+            this.$root.$emit('bv::disable::tooltip', element_name + '_error')
         },
         reset_link_sent(){
             alert("Link został wysłany na podany mail.")
