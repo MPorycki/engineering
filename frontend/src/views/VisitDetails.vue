4134284b697e8fe1ebf7be15f9b3fc565faf4c49
MPorycki <marcin.porycki@growbots.com>
2020-10-03 19:35:52 +0200

    Add visit details p1

---
+++ frontend/src/views/VisitDetails.vue
@@ -0,0 +1,33 @@
+<template>
+    <myForm>
+        <H2>Szczegóły wizyty</H2>
+        <div v-for="data in details" :key="data.id">
+            <p>{{data["field_name"]}}</p> <br>
+            <p>{{data["field_value"]}}</p>
+        </div>
+    </myForm>
+</template>
+
+<script>
+import myForm from '../components/myForm'
+
+import axios from 'axios'
+export default {
+    components: {
+        myForm
+    },
+    data(){
+        return {
+            visit: []
+        }
+    },
+    mounted() {
+        axios.get(this.$backend_url + "visit/"+this.$route.query.id).then(res => this.renderDetails(res.data))
+    },
+    methods: {
+        renderDetails(details){
+            console.log(details)
+        }
+    }
+}
+</script>
