d3f503786cbdc0d59dae62583ceea36c8c60b509
MPorycki <poryckim@gmail.com>
2020-11-13 09:54:17 +0100

    Main page first version

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="home">
+      <h1 id="welcome">Witamy na stronie naszych salonów</h1>
+      <router-link to="/login" class="btnSubmit" >Logowanie</router-link>
   </div>
 </template>
 
@@ -13,3 +15,48 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+body {
+  height: 100%
+}
+  img {
+    width: 100%;
+    height: 100%;
+  }
+  .home {
+    margin-bottom: -100px;
+    width: 100%;
+    min-height: 720px;
+    background-image: url(../assets/hairdress1.jpeg);
+    border-style: dotted;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  #welcome{
+    text-align: center;
+    width: 100%;
+    height: 100%;
+  }
+  .btnSubmit
+  {
+    width: 20%;
+    border-radius: 1rem;
+    padding: 1.5%;
+    border: none;
+    cursor: pointer;
+  }
+.btnSubmit{
+    font-weight: 800;
+    font-size: 25px;
+    color: #6998a3;
+    background-color: #a4e6f4;
+  }
+.btnSubmit:hover{
+    color: #fff;
+    background-color: #f3bac3;
+  }
+</style>
