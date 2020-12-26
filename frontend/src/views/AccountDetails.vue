1cf664abd5399d69e7f4c42ddf412c45cd66e7cb
MPorycki <marcin.porycki@growbots.com>
2020-12-26 14:40:46 +0100

    Add accounts details view

--- frontend/src/views/AccountDetails.vue
+++ frontend/src/views/AccountDetails.vue
@@ -1,17 +1,36 @@
 <template>
-    <div><p>Hello world</p></div>
+    <myForm>
+        <H2>Twoje konto</H2>
+        <h5><strong>Imię</strong></h5>
+        <p>{{details.firstName}}</p>
+        <h5><strong>Nazwisko</strong></h5>
+        <p>{{details.lastName}}</p>
+        <h5><strong>Email</strong></h5>
+        <p>{{details.email}}</p>
+    </myForm>
 </template>
 
 <script>
+import myForm from '../components/myForm'
+
 import axios from 'axios'
 export default {
+    components: {
+        myForm
+    },
+    data(){
+        return {
+            details: [],
+            id: ""
+        }
+    },
     mounted(){
     var config = { headers: {account_id: this.$cookies.get('user-id'), session_id: this.$cookies.get('session-id')}}
-    axios.get(this.$backend_url + "account/" + this.$cookies.get('user-id'), config).then(res => this.setDetails(res.data["account"]))
+    axios.get(this.$backend_url + "account/" + this.$cookies.get('user-id'), config).then(res => this.setDetails(res.data))
     },
     methods: {
         setDetails(data){
-            console.log(data)
+            this.details = data
         }
     }
     
