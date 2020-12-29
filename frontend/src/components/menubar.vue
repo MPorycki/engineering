fc9ac00f2534fa805a4f36e9b456ae6345a1f532
MPorycki <marcin.porycki@growbots.com>
2020-12-29 16:06:44 +0100

    Add client menu button to menubar

--- frontend/src/components/menubar.vue
+++ frontend/src/components/menubar.vue
@@ -15,13 +15,16 @@
             <li v-if="this.isAdmin == true">
                <a href="http://localhost:5000/admin/" target="_blank">Admin</a>
             </li>
+            <li v-if="this.isHairdresser == true">
+                <router-link to="/allCustomers">Klienci</router-link>
+            </li>
             <li>
                 <router-link to="/allSalons">Salony</router-link>
             </li>
             <li v-if="this.sessionId != ''">
                 <router-link to="/visitsAll">Moje wizyty</router-link>
             </li>
-            <li v-if="this.sessionId != ''">
+            <li v-if="this.sessionId != '' && this.isHairdresser == false">
                 <router-link to="/visitCreate">Umów się</router-link>
             </li>
             <li v-else v-on:click="redirectToLogin()">
@@ -42,7 +45,8 @@ export default {
         return {
             userId: "",
             sessionId: "",
-            isAdmin: false
+            isAdmin: false,
+            isHairdresser: false
         }
     },
     methods: {
@@ -54,12 +58,13 @@ export default {
             this.userId = this.$cookies.get('user-id')
             this.sessionId = this.$cookies.get('session-id')
             var config = { headers: {account_id: this.$cookies.get('user-id'), session_id: this.$cookies.get('session-id')}}
-            axios.get(this.$backend_url + "admin_access/", config).then(() => this.setIsAdmin()).catch(() => this.handleAdminError())
+            axios.get(this.$backend_url + "employee_access/", config).then(res => this.setAccess(res.data)).catch(() => this.handleAccessError())
         },
-        setIsAdmin(){
-            this.isAdmin = true
+        setAccess(accessResults){
+            this.isHairdresser = accessResults.isHairdresser
+            this.isAdmin = accessResults.isAdmin
         },
-        handleAdminError(){},
+        handleAccessError(){},
         sessionInvalid(){
             document.cookie = "session-id=;"
             document.cookie = "user-id=;"
