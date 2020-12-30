c741e8a34002ff937376fb3553d36905163de39e
MPorycki <marcin.porycki@growbots.com>
2020-12-30 11:18:35 +0100

    Remove create visit button from all visits for hairdressers

--- frontend/src/views/AllVisits.vue
+++ frontend/src/views/AllVisits.vue
@@ -1,18 +1,19 @@
 <template>
     <myForm>
         <h2>Moje wizyty</h2>
-        <router-link to='/visitCreate' class="btnSubmit" >Umów wizytę </router-link>
-
-           <table class="col text-center" id="myTable" v-for="visit in visits" :key="visit.visit_id">
-                <router-link tag="tr" id="tblrow" class="btn btn-light shadow" :to="{ path: 'visitDetails', query: { id: visit.visit_id }}">
-                    <td id="date">
-                        {{visit.visit_date}}
-                    </td>
-                    <td >
-                        {{visit.visit_data}}
-                    </td>
-                </router-link>
-            </table>
+        <router-link v-if="this.isHairdresser == false" to='/visitCreate' class="btnSubmit" >
+        Umów wizytę 
+        </router-link>
+        <table class="col text-center" id="myTable" v-for="visit in visits" :key="visit.visit_id">
+            <router-link tag="tr" id="tblrow" class="btn btn-light shadow" :to="{ path: 'visitDetails', query: { id: visit.visit_id }}">
+                <td id="date">
+                    {{visit.visit_date}}
+                </td>
+                <td >
+                    {{visit.visit_data}}
+                </td>
+            </router-link>
+        </table>
     </myForm>
 </template>
 
@@ -27,19 +28,25 @@ export default {
     },
     data(){
         return {
-            visits: []
+            visits: [],
+            ifHairdresser: false
         }
     },
     mounted(){
         var config = { headers: {account_id: this.$cookies.get('user-id'), session_id: this.$cookies.get('session-id')}}
         axios.get(this.$backend_url + "accountVisits/", config).then(res => this.setVisits(res.data["visits"]))
+        axios.get(this.$backend_url + "employee_access/", config).then(res => this.setAccess(res.data)).catch(() => this.handleAccessError())
     },
     methods: {
         setVisits(visitsInput){
             for (var i=0; i < visitsInput.length; i++) {
                 this.visits.push(visitsInput[i])
             }
-        }
+        },
+        setAccess(accessResults){
+            this.isHairdresser = accessResults.isHairdresser
+        },
+        handleAccessError(){}
     }
 }
 </script>
