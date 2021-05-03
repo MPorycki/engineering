6886d2574bc905d6d78966c81cb4acb9d8b43d0e
MPorycki <poryckim@gmail.com>
2021-05-03 08:33:31 +0100

    Add empty tests to test_adresses.py

---
+++ backend/Tests/Unit/test_adresses.py
@@ -0,0 +1,36 @@
+
+
+def test_validate_city_does_not_allow_bad_characters():
+    pass
+
+
+def test_validate_city_does_not_allow_too_many_words():
+    pass
+
+
+def test_validate_zip_does_not_allow_wrong_format():
+    pass
+
+
+def test_validate_street_does_not_allow_empty_street_name():
+    pass
+
+
+def test_validate_street_does_not_allow_too_long_street_name():
+    pass
+
+
+def test_validate_street_does_not_allow_bad_characters():
+    pass
+
+
+def test_validate_building_no_does_not_allow_empty_number():
+    pass
+
+
+def test_validate_building_no_does_not_allow_wrong_format():
+    pass
+
+
+def test_adress_to_string_has_correct_format():
+    pass
