16e340d14f4711b9babc472c5c6b456d5335fcf0
MPorycki <poryckim@gmail.com>
2021-05-18 20:43:51 +0100

    Add visit functional test part 2

--- backend/Tests/Functional/test_db_setup.py
+++ backend/Tests/Functional/test_db_setup.py
@@ -5,6 +5,14 @@ from models import Accounts, Adresses, Salons
 
 
 def set_up_test_db(session):
+    add_salons(session)
+    add_customers(session)
+    add_hairdressers(session)
+    add_services(session)
+
+
+
+def add_customers(session):
     session.add(Accounts(
         id=uuid.uuid4().hex,
         email="marcopolo@test.com",
@@ -15,10 +23,50 @@ def set_up_test_db(session):
         account_type="customer",
         salon_id=None
     ))
-    adress_id = uuid.uuid4().hex
-    salon_id = uuid.uuid4().hex
+
+
+def add_hairdressers(session):
+    session.add(Accounts(
+        id=uuid.uuid4().hex,
+        email="jan@testowy.com",
+        hashed_password="anything",
+        created_at=datetime.datetime(2020, 5, 17),
+        first_name="Jan",
+        last_name="Testowy",
+        account_type="hairdresser",
+        salon_id="219556b04e104ba2b19486cb7fda677a"
+    ))
+    session.add(Accounts(
+        id=uuid.uuid4().hex,
+        email="marcin@gmail.com",
+        hashed_password="anything",
+        created_at=datetime.datetime(2020, 5, 17),
+        first_name="Marcin",
+        last_name="Testowin",
+        account_type="hairdresser",
+        salon_id="219556b04e104ba2b19486cb7fda677a"
+    ))
+    session.add(Accounts(
+        id=uuid.uuid4().hex,
+        email="krzys.nowak@test.com",
+        hashed_password="anything",
+        created_at=datetime.datetime(2020, 5, 17),
+        first_name="Krzysztof",
+        last_name="Nowak",
+        account_type="hairdresser",
+        salon_id="219556b04e104ba2b19486cb7fda677a"
+    ))
+
+
+def add_salons(session):
+    adress_id = "c988c9eb72484455a07ef15351e2030d"
+    salon_id = "219556b04e104ba2b19486cb7fda677a"
     session.add(Adresses(id=adress_id, city="Warsaw", street="Marszalkowska", building_no=3,
                          salon_id=salon_id, zip_code="05-822", number_of_seats=5,
                          created_at=datetime.datetime.utcnow()))
     session.add(Salons(id=salon_id, adress_id=adress_id, opening_hour="09:00",
                        closing_hour="17:00", created_at=datetime.datetime.utcnow()))
+
+
+def add_services(session):
+    pass
--- backend/Tests/Functional/tests_functional.py
+++ backend/Tests/Functional/tests_functional.py
@@ -1,3 +1,5 @@
+import json
+
 from pytest_mock_resources import create_sqlite_fixture
 from pytest import fixture
 
@@ -29,8 +31,28 @@ def test_base_connection(client):
 def test_register_for_a_visit(sq, client, monkeypatch):
     monkeypatch.setattr("models.Session", lambda: sq)
     set_up_test_db(sq)
-    # User picks the salon
-    print(show_user(sq).first_name)
+    # User enters the registration page. List of available salons loads
     rv = client.get("/salon/")
-    print(rv.data)
+    salon_data = json.loads(rv.data.decode('utf8'))
+    print(rv.data.decode('utf8'))
+    assert len(salon_data["Salons"]) == 1
+    assert "city" in salon_data["Salons"][0]["address"]
+    assert salon_data["Salons"][0]["address"]["city"] == "Warsaw"
+    assert "street" in salon_data["Salons"][0]["address"]
+    assert salon_data["Salons"][0]["address"]["street"] == "Marszalkowska"
+    # User picks the salon
+    picked_salon = salon_data["Salons"][0]
+    # The list of available hairdressers loads
+    hairdressers_request = client.get("/hairdresser/" + picked_salon["id"])
+    print(hairdressers_request.data)
+    hairdressers_data = json.loads(hairdressers_request.data.decode('utf8'))["Hairdressers"]
+    assert len(hairdressers_data) == 3
+    assert "first_name", "last_name" in hairdressers_data[0]
+    assert hairdressers_data[0]["firstName"] == "Jan"
+    assert hairdressers_data[1]["firstName"] == "Marcin"
     # User picks one of the available hairdressers
+    picked_hairdresser = hairdressers_data[1]
+    # List of available services is loaded
+    services_request = client.get("/service/")
+    print(services_request.data)
+    services_data = json.loads(services_request.data.decode('utf8'))["Services"]
