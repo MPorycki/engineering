51d123af5aa7546f73b1dd59a9aa8fcfbb52a7dc
MPorycki <poryckim@gmail.com>
2021-07-05 21:24:29 +0200

    Remove not needed validation for services, salons

--- backend/validation.py
+++ backend/validation.py
@@ -4,55 +4,6 @@ from flask_inputs.validators import JsonSchema
 # https://python-jsonschema.readthedocs.io/en/stable/
 # https://json-schema.org/draft-07/json-schema-validation.html#rfc.section.3.2.1
 
-service_schema = {
-    'type': 'object',
-    'properties': {
-        'name': {
-            'type': 'string'
-        },
-        'price': {
-            'type': 'number'
-        },
-        'description': {
-            'type': 'string'
-        },
-        'gender': {
-            'type': 'string'
-        },
-        'service_duration': {
-            'type': 'number'
-        }
-    },
-    'required': ['name', 'price', 'description', 'gender', 'service_duration']
-}
-
-
-class ServiceInputs(Inputs):
-    json = [JsonSchema(schema=service_schema)]
-
-
-salon_schema = {
-    'type': 'object',
-    'properties': {
-        'opening_hour': {
-            'type': 'string',
-            'pattern': r'[0-9]{2}:[0-9]{2}'
-        },
-        'closing_hour': {
-            'type': 'string',
-            'pattern': r'[0-9]{2}:[0-9]{2}'
-        },
-        'adress': {
-            'type': 'object',
-        },
-    },
-    'required': ['opening_hour', 'closing_hour', 'adress']
-}
-
-
-class SalonInputs(Inputs):
-    json = [JsonSchema(schema=salon_schema)]
-
 
 visit_schema = {
     'type': 'object',
